f44b42fa65e2f755fad54cfbf189e3b6a41c1c43
ubterzioglu <ubterzioglu@gmail.com>
2025-12-18 21:41:29 +0100

    GOAT To all!

---
+++ goat.js
@@ -0,0 +1,13 @@
+(function () {
+  if (window.__GOAT_LOADED__) return;
+  window.__GOAT_LOADED__ = true;
+
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "//gc.zgo.at/count.js";
+  s.setAttribute(
+    "data-goatcounter",
+    "https://ubterzioglude.goatcounter.com/count"
+  );
+  document.head.appendChild(s);
+})();
