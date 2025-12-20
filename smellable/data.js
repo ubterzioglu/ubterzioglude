ff94ebd80783b8fa9527df94558b5f29ef79c065
ubterzioglu <ubterzioglu@gmail.com>
2025-12-20 16:13:58 +0100

    Smellable fine tunings!

--- smellable/data.js
+++ smellable/data.js
@@ -2,7 +2,7 @@ const smells = [
     {
         id: "smell_001",
         name: "Freshly cut grass",
-        image: "/img/grass.jpg",
+        image: "img/grass.jpg",
         category: "nature",
         reactions: {
             yes: 0,
@@ -13,7 +13,7 @@ const smells = [
     {
         id: "smell_002",
         name: "Coffee in the morning",
-        image: "/img/coffee.jpg",
+        image: "img/coffee.jpg",
         category: "food",
         reactions: {
             yes: 0,
@@ -24,7 +24,7 @@ const smells = [
     {
         id: "smell_003",
         name: "Old books",
-        image: "/img/books.jpg",
+        image: "img/books.jpg",
         category: "nostalgia",
         reactions: {
             yes: 0,
@@ -35,7 +35,7 @@ const smells = [
     {
         id: "smell_004",
         name: "Gasoline",
-        image: "/img/gasoline.jpg",
+        image: "img/gasoline.jpg",
         category: "industrial",
         reactions: {
             yes: 0,
@@ -46,7 +46,7 @@ const smells = [
     {
         id: "smell_005",
         name: "Rain on warm asphalt",
-        image: "/img/rain.jpg",
+        image: "img/rain.jpg",
         category: "nature",
         reactions: {
             yes: 0,
@@ -57,7 +57,7 @@ const smells = [
     {
         id: "smell_006",
         name: "Sunscreen at the beach",
-        image: "/img/sunscreen.jpg",
+        image: "img/sunscreen.jpg",
         category: "summer",
         reactions: {
             yes: 0,
